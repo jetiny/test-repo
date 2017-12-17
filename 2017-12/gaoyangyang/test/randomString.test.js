16a76a46583e797643f6afa233c005c7163e1def
alien <13373935140@163.com>
2017-12-17 23:12:58 +0800

    symbols

--- 2017-12/gaoyangyang/test/randomString.test.js
+++ 2017-12/gaoyangyang/test/randomString.test.js
@@ -6,6 +6,6 @@ test('randomString', t => {
   // should return the default string
   expect(randomString()).to.be.a('string')
   // symbols 参数
-  expect(randomString("","",1).length).to.eql(0)  
+  expect(randomString("","",1)).to.eql('')  
   t.pass()
 })
